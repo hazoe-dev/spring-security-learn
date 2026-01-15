f45ccd2eb59aa21a0f2d1c68f7aa950d9657412c
hazoe-dev <hatran.dev@gmail.com>
2026-01-15 16:04:57 +0700

    [Enhance] apply bcrypt encoder for registering user.

--- src/main/java/dev/hazoe/springsecuritydemo/config/SecurityConfiguration.java
+++ src/main/java/dev/hazoe/springsecuritydemo/config/SecurityConfiguration.java
@@ -11,7 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,6 +24,11 @@ public class SecurityConfiguration {
     @Autowired
     public SecurityConfiguration(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12); //2^12 rounds
     }
 
     @Bean
--- src/main/java/dev/hazoe/springsecuritydemo/service/UserService.java
+++ src/main/java/dev/hazoe/springsecuritydemo/service/UserService.java
@@ -6,21 +6,24 @@ import dev.hazoe.springsecuritydemo.model.User;
 import dev.hazoe.springsecuritydemo.model.dto.UserRequest;
 import dev.hazoe.springsecuritydemo.model.dto.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private UserRepo userRepo;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public void setUserRepo(UserRepo userRepo) {
+    public UserService(UserRepo userRepo, PasswordEncoder passwordEncoder) {
         this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserResponse save(UserRequest user) {
         User newUser = new User();
         newUser.setUsername(user.username());
-        newUser.setPassword(user.password());
+        newUser.setPassword(passwordEncoder.encode(user.password()));
         newUser.setRole(Role.USER);
         User savedUser = userRepo.save(newUser);
         return new UserResponse(savedUser.getUsername());
