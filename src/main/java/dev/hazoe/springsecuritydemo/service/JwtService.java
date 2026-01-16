b039621152b659d22bd7e7877c54fca8547b2a86
hazoe-dev <hatran.dev@gmail.com>
2026-01-16 22:40:10 +0700

    [Update] generating token.

--- src/main/java/dev/hazoe/springsecuritydemo/service/JwtService.java
+++ src/main/java/dev/hazoe/springsecuritydemo/service/JwtService.java
@@ -6,48 +6,28 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class JwtService {
-    private static final String SECRET = "TmV3U2VjcmV0S2V5Rm9ySldUU29uZ2luZ21hc2sgZmF2b3I=";
-    private String secretKey;
+    //The same at all times
+    private final Key key;
 
     public JwtService() {
-        this.secretKey = generateSecretKey();
-    }
-
-    public String generateSecretKey() {
-        try {
-            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
-            keyGenerator.init(256);
-            SecretKey generateKey = keyGenerator.generateKey();
-            return Base64.getEncoder().encodeToString(generateKey.getEncoded());
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Error generating secret key" + e);
-        }
+        byte[] keyBytes = Decoders.BASE64.decode(
+                "TmV3U2VjcmV0S2V5Rm9ySldUU29uZ2luZ21hc2sgZmF2b3I="
+        );
+        this.key = Keys.hmacShaKeyFor(keyBytes);
     }
 
     public String getAccessToken(String username) {
-        Map<String, Object> claims = new HashMap<>();
         return Jwts.builder()
-                .claims(claims)
                 .subject(username)
-                .issuedAt(new Date(System.currentTimeMillis()))
+                .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 3))
-                .signWith(SignatureAlgorithm.HS256, getKey())
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    private Key getKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
 }
